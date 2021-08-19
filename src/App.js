88ecb7df1d5be9e0391282c6db581c8ce3900624
sharma66mahesh <gauravpudel1990@gmail.com>
2021-08-19 13:00:17 +0545

    :sparkles: Send did+email+wallet to backend for verification

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import { Magic } from 'magic-sdk';
+import { IconExtension } from '@magic-ext/icon';
+import { useState } from 'react';
+
+const BACKEND_URL = 'http://localhost:5000';
+
+const magic = new Magic('pk_test_221FCC565FA90776', {
+  extensions: [
+    new IconExtension({
+      rpcUrl: 'https://bicon.net.solidwallet.io/api/v3',
+    }),
+  ],
+});
 
 function App() {
+
+  const [userMetadata, setUserMetadata] = useState(null);
+  const [enteredEmail, setEnteredEmail] = useState('');
+
+  const loginUser = async (e) => {
+    try {
+      e.preventDefault();
+      await magic.auth.loginWithMagicLink({
+          email: enteredEmail,
+          showUI: true
+      });
+      const metadata = await magic.user.getMetadata();
+      console.log(metadata);
+      setUserMetadata(metadata);
+    } catch(e) {
+      console.group('ERROR LOGGING IN WITH MAGIC');
+      console.error(e);
+      console.groupEnd();
+    }
+  }
+
+  const verifyClaim = async () => {
+    // generate DID token with email and walletAddress signature in it
+    const userObj = {
+      email: userMetadata.email,
+      walletAddress: userMetadata.publicAddress
+    };
+    const idToken = await magic.user.generateIdToken({
+      attachment: JSON.stringify(userObj)
+    });
+
+    console.log("-----------GENERATED ID TOKEN IS-------------");
+    console.log(idToken);
+
+    // send the id token to backend for user verification
+    try {
+      const response = await fetch(BACKEND_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+          idToken,
+          additionalData: JSON.stringify(userObj)
+        })
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.group('POST DID TO BACKEND');
+      console.error(e);
+      console.groupEnd();
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={loginUser}>
+        <input type='text' name='email' value={enteredEmail} onChange={(e) => setEnteredEmail(e.target.value.trim())} />
+        <button disabled={userMetadata !== null ? true : false} type='submit'>Login</button>
+      </form>
+      <br/><br/><br/>
+      <button disabled={userMetadata === null ? true : false} onClick={verifyClaim}>Verify data</button>
     </div>
   );
 }
